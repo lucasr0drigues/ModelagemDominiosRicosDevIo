a3219f3981f6310a67a48924e5131b6c1787de98
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-06-30 00:57:08 -0300

    creating project structure and initial entities

---
+++ NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,48 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Guid CategoriaId { get; private set; }
+        public Categoria Categoria { get; private set; }
+
+        public Produto(
+            string nome,
+            string descricao,
+            bool ativo,
+            decimal valor,
+            DateTime dataCadastro,
+            string imagem,
+            int quantidadeEstoque,
+            Guid categoriaId,
+            Categoria categoria)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+            QuantidadeEstoque = quantidadeEstoque;
+            CategoriaId = categoriaId;
+            Categoria = categoria;
+        }
+    }
+
+    public class Categoria : Entity
+    {
+    }
+}
