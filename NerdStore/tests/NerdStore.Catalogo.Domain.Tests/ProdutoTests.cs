98d7d840d61f5a8b24456d19c6dab00b9a06f081
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-07-01 01:10:30 -0300

    add entity mapping

--- NerdStore/tests/NerdStore.Catalogo.Domain.Tests/ProdutoTests.cs
+++ NerdStore/tests/NerdStore.Catalogo.Domain.Tests/ProdutoTests.cs
@@ -18,8 +18,7 @@ namespace NerdStore.Catalogo.Domain.Tests
                     Guid.NewGuid(),
                     DateTime.Now,
                     "Imagem",
-                    new Dimensoes(1, 1, 1))
-            );
+                    new Dimensoes(1, 1, 1)));
 
             Assert.Equal("O campo Nome do produto não pode estar vazio", ex.Message);
 
@@ -32,8 +31,7 @@ namespace NerdStore.Catalogo.Domain.Tests
                     Guid.NewGuid(),
                     DateTime.Now,
                     "Imagem",
-                    new Dimensoes(1, 1, 1))
-            );
+                    new Dimensoes(1, 1, 1)));
 
             Assert.Equal("O campo Descricao do produto não pode estar vazio", ex.Message);
 
@@ -46,8 +44,7 @@ namespace NerdStore.Catalogo.Domain.Tests
                     Guid.NewGuid(),
                     DateTime.Now,
                     "Imagem",
-                    new Dimensoes(1, 1, 1))
-            );
+                    new Dimensoes(1, 1, 1)));
 
             Assert.Equal("O campo Valor do produto não pode se menor igual a 0", ex.Message);
 
@@ -60,8 +57,7 @@ namespace NerdStore.Catalogo.Domain.Tests
                     Guid.Empty,
                     DateTime.Now,
                     "Imagem",
-                    new Dimensoes(1, 1, 1))
-            );
+                    new Dimensoes(1, 1, 1)));
 
             Assert.Equal("O campo CategoriaId do produto não pode estar vazio", ex.Message);
 
@@ -74,8 +70,7 @@ namespace NerdStore.Catalogo.Domain.Tests
                     Guid.NewGuid(),
                     DateTime.Now,
                     string.Empty,
-                    new Dimensoes(1, 1, 1))
-            );
+                    new Dimensoes(1, 1, 1)));
 
             Assert.Equal("O campo Imagem do produto não pode estar vazio", ex.Message);
 
@@ -88,8 +83,7 @@ namespace NerdStore.Catalogo.Domain.Tests
                     Guid.NewGuid(),
                     DateTime.Now,
                     "Imagem",
-                    new Dimensoes(0, 1, 1))
-            );
+                    new Dimensoes(0, 1, 1)));
 
             Assert.Equal("O campo Altura não pode ser menor ou igual a 0", ex.Message);
         }
