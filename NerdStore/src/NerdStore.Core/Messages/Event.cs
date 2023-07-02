ad0f86b059b9752ca9029131943b931e6a1278b4
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-07-02 05:17:38 -0300

    add produto abaixo estoque event

---
+++ NerdStore/src/NerdStore.Core/Messages/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace NerdStore.Core.Messages
+{
+    public abstract class Event : Message
+    {
+        public DateTime Timestamp { get; private set; }
+
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
