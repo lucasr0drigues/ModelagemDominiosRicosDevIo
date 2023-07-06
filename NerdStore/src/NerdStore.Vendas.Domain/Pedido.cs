0156a4d67f51ee7c78f5505638a36fb9cee3fb62
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-07-05 23:48:07 -0300

    add vendas projects

---
+++ NerdStore/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido : Entity
+    {
+    }
+}
