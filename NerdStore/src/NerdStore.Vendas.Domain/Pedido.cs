4454a546d965ce39256e6e20b118a88dbdf00538
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2023-07-10 06:10:43 -0300

    add status methods

--- NerdStore/src/NerdStore.Vendas.Domain/Pedido.cs
+++ NerdStore/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -25,6 +25,12 @@ namespace NerdStore.Vendas.Domain
         // EF Relacionamentos
         public virtual Voucher Voucher { get; private set; }
 
+
+        protected Pedido() 
+        {
+            _pedidoItens = new List<PedidoItem>();
+        }
+
         public Pedido(
             Guid clienteId,
             bool voucherUtilizado,
@@ -35,6 +41,132 @@ namespace NerdStore.Vendas.Domain
             VoucherUtilizado = voucherUtilizado;
             Desconto = desconto;
             ValorTotal = valorTotal;
+            _pedidoItens = new List<PedidoItem>();
+        }
+
+        public void AplicarVoucher(Voucher voucher)
+        {
+            Voucher = voucher;
+            VoucherUtilizado = true;
+            CalcularValorPedido();
+        }
+
+        public void CalcularValorPedido()
+        {
+            ValorTotal = PedidoItens.Sum(p => p.CalcularValor());
+            CalcularValorTotalDesconto();
+        }
+
+        public void CalcularValorTotalDesconto()
+        {
+            if (!VoucherUtilizado) return;
+
+            decimal desconto = 0;
+            var valor = ValorTotal;
+
+            if(Voucher.TipoDescontoVoucher == TipoDescontoVoucher.Porcentagem)
+            {
+                if(Voucher.Percentual.HasValue)
+                {
+                    desconto = (valor * Voucher.Percentual.Value) / 100;
+                    valor -= desconto;
+                }
+            }
+            else
+            {
+                if(Voucher.ValorDesconto.HasValue)
+                {
+                    desconto = Voucher.ValorDesconto.Value;
+                    valor -= desconto;
+                }
+            }
+
+            ValorTotal = valor < 0 ? 0 : valor;
+            Desconto = desconto;
+        }
+
+        public bool PedidoItemExistente(PedidoItem item)
+        {
+            return _pedidoItens.Any(p => p.ProdutoId == item.ProdutoId);
+        }
+
+        public void AdicionarItem(PedidoItem item)
+        {
+            if (!item.EhValido()) return;
+
+            item.AssociarPedido(Id);
+
+            if(PedidoItemExistente(item))
+            {
+                var itemExistente = _pedidoItens.FirstOrDefault(p => p.ProdutoId == item.ProdutoId);
+                itemExistente.AdicionarUnidades(item.Quantidade);
+                item = itemExistente;
+
+                _pedidoItens.Remove(itemExistente);
+            }
+
+            item.CalcularValor();
+            _pedidoItens.Add(item);
+        }
+
+        public override bool EhValido()
+        {
+            return true;
+        }
+
+        public void RemoverItem(PedidoItem item)
+        {
+            if(!item.EhValido()) return;
+
+            var itemExistente = PedidoItens.FirstOrDefault(p => p.ProdutoId == item.ProdutoId);
+
+            if (itemExistente == null) throw new DomainException("O item nao percente ao pedido");
+
+            _pedidoItens.Remove(itemExistente);
+
+            CalcularValorPedido();
+        }
+
+        public void AtualizarItem(PedidoItem item)
+        {
+            if (!item.EhValido()) return;
+
+            item.AssociarPedido(Id);
+
+            var itemExistente = PedidoItens.FirstOrDefault(p => p.ProdutoId == item.ProdutoId);
+
+            if (itemExistente == null) throw new DomainException("O item nao pertence ao pedido");
+
+            _pedidoItens.Remove(itemExistente);
+            _pedidoItens.Add(item);
+
+            CalcularValorPedido();
+        }
+
+        public void AtualizarUnidades(PedidoItem item, int unidades)
+        {
+            item.AtualizarUnidades(unidades);
+            AtualizarItem(item);
+        }
+
+        public void TornarRascunho()
+        {
+            PedidoStatus = PedidoStatus.Rascunho;
+        }
+
+        public void IniciarPedido()
+        {
+            PedidoStatus = PedidoStatus.Iniciado;
+        }
+
+        public void FinalizarPedido()
+        {
+            PedidoStatus = PedidoStatus.Pago;
+        }
+
+        public void CancelarPedido()
+        {
+            PedidoStatus = PedidoStatus.Cancelado;
         }
     }
 }
--- NerdStore/src/NerdStore.Vendas.Domain/PedidoItem.cs
+++ NerdStore/src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -21,13 +21,11 @@ namespace NerdStore.Vendas.Domain
         protected PedidoItem() { }
 
         public PedidoItem(
-            Guid pedidoId,
             Guid produtoId,
             string produtoNome,
             int quantidade,
             decimal valorUnitario)
         {
-            PedidoId = pedidoId;
             ProdutoId = produtoId;
             ProdutoNome = produtoNome;
             Quantidade = quantidade;
