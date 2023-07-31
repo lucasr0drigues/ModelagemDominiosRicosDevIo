221f70a66242a8c8f061e1ca53882550b147595a
lucas rodrigues <lucas.a.rodrigues@outlook.com>
2023-07-31 00:10:51 -0300

    initiate pedido command handler

--- NerdStore/src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
+++ NerdStore/src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MediatR;
+using NerdStore.Core.Messages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace NerdStore.Vendas.Application.Commands
 {
-    internal class PedidoCommandHandler
+    public class PedidoCommandHandler : IRequestHandler<AdicionarItemPedidoCommand, bool>
     {
+        public async Task<bool> Handle(AdicionarItemPedidoCommand message, CancellationToken cancellationToken)
+        {
+            if(!ValidarComando(message)) return false;
+
+            return true;
+        }
+
+        private bool ValidarComando(Command message)
+        {
+            if (message.EhValido()) return true;
+
+            foreach (var error in message.ValidationResult.Errors)
+            {
+                // lancar evento de erro
+            }
+
+            return false;
+        }
     }
 }
